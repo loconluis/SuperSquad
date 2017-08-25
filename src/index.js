4d8c1e02bfbff7d61bef7f21fa739de2bf462d4a
luislocon <luis.locon@gmail.com>
2017-08-25 12:21:49 -0600

    Adding redux bases Action, Reducers, Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import rootReducer from './reducers'
+import { addCharacterById } from './actions';
+
+const store = createStore(rootReducer);
+console.log('store: ', store.getState());
+
+store.subscribe( () => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
